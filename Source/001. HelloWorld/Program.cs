42aa993020089a05a9b9c48bb1f35ef77b40587b
Luke Malpass <contact@angelsix.com>
2020-02-13 07:36:51 +0000

    Hello World and Syntax

---
+++ Source/001. HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 1!");
+            Console.WriteLine("Hello World 2!");
+            Console.WriteLine("Hello World 3!");
+            Console.WriteLine("Hello World 4!");
+            Console.WriteLine("Hello World 5!");
+            Console.WriteLine("Hello World 6!");
+        }
+    }
+}
---
+++ Source/002. Syntax/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; // using directive
+
+namespace HelloWorld // Namespace declaration
+{ // Start namespace block
+    class Program // Create class
+    { // Start class block
+        static void Main(string[] args) // Create method
+        { // Start method block
+            Console
+                .
+                WriteLine
+                (
+                "Hello " +
+                "World 1!"
+                ); // Expression statement
+            Console.WriteLine("Hello World 2!"); // Expression statement
+            Console.WriteLine("Hello World 3!"); // Expression statement
+            Console.WriteLine("Hello World 4!"); // Expression statement
+            Console.WriteLine("Hello World 5!"); // Expression statement
+            Console.WriteLine("Hello World 6!"); // Expression statement
+        } // End method block
+    } // End class block
+} // End namespace block
+
+//
+// RULES
+//
+//   1. Using directives and statements end with ; 
+// 
+//   2. Namespaces, classes, methods use { }
+//
