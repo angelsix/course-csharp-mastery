d9e687e618f76331ea50aaa800f59fb517cb1843
Luke Malpass <contact@angelsix.com>
2020-02-27 15:11:17 +0000

    Integers

---
+++ Source/003. Integers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Declaring and assign variable
+            int counter = 5;
+
+            // Output value to console
+            Console.WriteLine("Counter is " + counter);
+
+            // Change counter to 7
+            counter = 7;
+
+            // Output value to console
+            Console.WriteLine("Counter is " + counter);
+
+            // Increment counter by 1
+            counter = counter + 1;
+
+            // Output value to console
+            Console.WriteLine("Counter is " + counter);
+
+            // Increment counter by 1
+            counter++;
+
+            // Output value to console
+            Console.WriteLine("Counter is " + counter);
+
+            // Decrement counter by 1
+            counter--;
+
+            // Output value to console
+            Console.WriteLine("Counter is " + counter);
+
+            // Multiply by 3
+            counter *= 3;
+
+            // Output value to console
+            Console.WriteLine("Counter is " + counter);
+
+            // Divide by 3
+            counter /= 3;
+
+            // Output value to console
+            Console.WriteLine("Counter is " + counter);
+
+            // Modulus of 2
+            counter %= 2;
+
+            // Output value to console
+            Console.WriteLine("Counter is " + counter);
+        }
+    }
+}
