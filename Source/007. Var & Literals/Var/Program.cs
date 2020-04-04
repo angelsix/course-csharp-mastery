ebcd8dc0c81b4da5ae7f7a25f10f83489b7ff138
Luke Malpass <contact@angelsix.com>
2020-04-04 08:44:35 +0100

    Methods. debugging and var

---
+++ Source/007. Var & Literals/Var/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var myByte = 0b0101;
+            var mySbyte = -0b0101;
+            var myShort = (short)1;
+            var myUshort = (ushort)1;
+            var myInt = 1;
+            var myUint = (uint)1;
+            var myLong = 1L;
+            var myUlong = 1UL;
+            var myFloat = 1.1F;
+            var myDouble = 1.1;
+            var myDecimal = 1.1M;
+
+            var myBool = true;
+
+            var myChar = 'a';
+            var myString = "hello worlds";
+
+        }
+    }
+}
