cfd162fcf78502f6f4a90a9f972c18bcb4ff31e4
Luke Malpass <contact@angelsix.com>
2020-04-05 09:22:14 +0100

    Parsing

---
+++ Source/008. Parsing/BirthDayCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace BirthDayCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntroduceSavvy();
+            
+            var usersDateOfBirth = AskForDateOfBirth();
+        }
+
+        public static void IntroduceSavvy()
+        {
+            Console.WriteLine("Hi, I'm Savvy. " +
+                "I'm really good at guessing the " +
+                "day of the week you were born on.");
+        }
+
+        public static DateTimeOffset AskForDateOfBirth()
+        {
+            Console.WriteLine("Can I start by asking you " +
+                "what your date of birth is? " + 
+                CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern);
+
+            var userText = Console.ReadLine();
+
+            return DateTimeOffset.Parse(userText);
+        }
+    }
+}
